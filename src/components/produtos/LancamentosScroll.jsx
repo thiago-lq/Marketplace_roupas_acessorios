d2bea10aef79d5b8531ed10eac2eb95a1538968b
thiago-lq <thiagolq100@gmail.com>
2025-07-28 18:05:31 -0300

    mudança simples

--- src/components/produtos/LancamentosScroll.jsx
+++ src/components/produtos/LancamentosScroll.jsx
@@ -170,7 +170,7 @@ export default function LancamentosScroll({ products, onAddToCart }) {
 
           {/* Grid dos produtos */}
           <div className="overflow-y-auto snap-y snap-mandatory max-h-[90vh]">
-            <div className="grid grid-cols-2 md:grid-cols-4 md:gap-[15px] gap-3 md:p-10 transition-all duration-500">
+            <div className="grid grid-cols-2 md:grid-cols-4 md:gap-[15px] gap-3 md:p-10 p-3 transition-all duration-500">
               {produtosSubFiltrados.map((product) => (
                 <div
                   key={product.id}
