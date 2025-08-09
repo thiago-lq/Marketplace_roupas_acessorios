8f37adc476cfd6535197deded75d3c04d282b070
thiago-lq <thiagolq100@gmail.com>
2025-08-09 16:51:25 -0300

    correções de segurança

--- src/firebase/configs.js
+++ src/firebase/configs.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyBRKj7yeeNocStt4I8BzIXp-HwterMKao4",
-    authDomain: "db-acessorios.firebaseapp.com",
-    projectId: "db-acessorios",
-    storageBucket: "db-acessorios.firebasestorage.app",
-    messagingSenderId: "672681573022",
-    appId: "1:672681573022:web:3fa5978ce7a47ea3b4998f"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
